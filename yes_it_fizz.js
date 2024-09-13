c7b6f81038dd958904095ab2c57ad1f88648f441
arin-s <sarkisianarin@gmail.com>
2024-09-13 16:10:25 +1000

    fix up formatting

--- yes_it_fizz.js
+++ yes_it_fizz.js
@@ -2,17 +2,17 @@ var readline = require('readline');
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
- });
+});
 
-rl.on('line', function(line){
+rl.on('line', function (line) {
     let ints = line.split(" ").sort();
-    for(let i = ints[0]; i <= ints[1]; i++) {
+    for (let i = ints[0]; i <= ints[1]; i++) {
         let str = "";
-        if(i % 3 == 0)
+        if (i % 3 == 0)
             str += "Fizz";
-        if(i % 5 == 0)
+        if (i % 5 == 0)
             str += "Buzz";
-        if(str == "")
+        if (str == "")
             str = i;
         console.log(str);
     }
