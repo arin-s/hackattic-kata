8ef181163773327eae06be578299c9c3cb8eda08
arin-s <sarkisianarin@gmail.com>
2024-09-13 21:14:30 +1000

    fix formatting

--- sorting_json_lines.js
+++ sorting_json_lines.js
@@ -1,9 +1,34 @@
-let example = '{"Bradley": {"balance": 9574723, "account_number": 1848285}, "extra": {"balance": -6804429}}';
-let person = {name: null, balance: null};
-    let json = JSON.parse(example);
+var readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let people = [];
+let i = 0;
+
+rl.on('line', function (line) {
+    let person = { name: null, balance: null };
+    let json = JSON.parse(line);
     person.name = Object.keys(json)[0];
-    if(json[person.name].hasOwnProperty("extra"))
-        person.balance = json[person.name].extra.balance;
+    if (json.hasOwnProperty("extra"))
+        person.balance = json.extra.balance;
     else
         person.balance = json[person.name].balance;
-console.log(person.balance);
+    people.push(person);
+    //No way to know we've finished receiving lines other than by keeping count
+    i++;
+    if (i == 13)
+        processOutput();
+});
+
+function processOutput() {
+    people.sort((a, b) => {
+        if (a.balance > b.balance)
+            return 1;
+        else
+            return -1;
+    });
+    for (let person of people)
+        console.log(person.name + ": " + person.balance.toLocaleString("en"));
+}
