a2ed4ee6f65b16ea7a3212bdc8e0495839e78aa6
arin-s <sarkisianarin@gmail.com>
2024-09-13 15:18:29 +1000

    Yes it fizz complete

---
+++ Yes_it_fizz.js
@@ -0,0 +1,19 @@
+var readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+ });
+
+rl.on('line', function(line){
+    let ints = line.split(" ").sort();
+    for(let i = ints[0]; i <= ints[1]; i++) {
+        let str = "";
+        if(i % 3 == 0)
+            str += "Fizz";
+        if(i % 5 == 0)
+            str += "Buzz";
+        if(str == "")
+            str = i;
+        console.log(str);
+    }
+})
